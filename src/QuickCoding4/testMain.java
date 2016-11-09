46ea88e1afeac560cf0e416c1fc196e68e7582fa
kimhyein <hyein15@kookmin.ac.kr>
2016-11-09 23:29:59 +0900

    quickcoding4

--- src/QuickCoding4/testMain.java
+++ src/QuickCoding4/testMain.java
@@ -6,18 +6,20 @@ public class testMain {
 	public static void main (String []args) {
 	Thread depositMan = new Thread() {
 		public void run(){
-			
+			for(int i=0; i<10; i++)
+			{
 				account.add(10);	
-			
+			}
 		}
 	};
 	
 	Thread withdrawMan = new Thread() {
 		
 		public void run(){
-			
+			for(int i=0; i<10; i++)
+			{
 				account.delete(10);	
-			
+			}
 		}
 	};
 	
